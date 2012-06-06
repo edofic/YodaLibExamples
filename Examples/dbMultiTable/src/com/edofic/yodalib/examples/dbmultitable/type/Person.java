edfc479fcf5588abc77a4695ef835dfeb50919fe
Andraz Bajt <edofic@gmail.com>
2012-06-07 00:09:19 +0200

    copyright update

--- Examples/dbMultiTable/src/com/edofic/yodalib/examples/dbmultitable/type/Person.java
+++ Examples/dbMultiTable/src/com/edofic/yodalib/examples/dbmultitable/type/Person.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Andraz Bajt
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.edofic.yodalib.examples.dbmultitable.type;
 
 import com.edofic.yodalib.database.Column;
@@ -10,9 +25,12 @@ import com.edofic.yodalib.database.Table;
  */
 @Table
 public class Person {
-    @Column private long id;
-    @Column private String name;
-    @Column private String surname;
+    @Column
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
 
     public Person(long id, String name, String surname) {
         this.id = id;
@@ -20,7 +38,8 @@ public class Person {
         this.surname = surname;
     }
 
-    public Person() {}
+    public Person() {
+    }
 
     public long getId() {
         return id;
--- Examples/dbMultiTable/src/com/edofic/yodalib/examples/dbmultitable/type/Product.java
+++ Examples/dbMultiTable/src/com/edofic/yodalib/examples/dbmultitable/type/Product.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Andraz Bajt
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.edofic.yodalib.examples.dbmultitable.type;
 
 import com.edofic.yodalib.database.Column;
@@ -10,9 +25,12 @@ import com.edofic.yodalib.database.Table;
  */
 @Table
 public class Product {
-    @Column private long id;
-    @Column private String tag;
-    @Column private long price;
+    @Column
+    private long id;
+    @Column
+    private String tag;
+    @Column
+    private long price;
 
     public Product(long id, String tag, long price) {
         this.id = id;
@@ -20,7 +38,8 @@ public class Product {
         this.price = price;
     }
 
-    public Product() {}
+    public Product() {
+    }
 
     public long getId() {
         return id;
